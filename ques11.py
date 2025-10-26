74ae13303f5b59b0585280704ecfa90167194d3b
pp8191331-ux <pp8191331@gmail.com>
2025-10-26 22:49:45 +0530

    Add files via upload

---
+++ ques11.py
@@ -0,0 +1,18 @@
+works = int(input("Enter number of works: "))
+for i in range(works):
+    SH = int(input(f"Enter starting hour of work {i+1}: "))
+    SM = int(input(f"Enter starting minutes of work {i+1}: "))
+    EH = int(input(f"Enter ending hour of work {i+1}: "))
+    EM = int(input(f"Enter ending minutes of work {i+1}: "))
+
+    start = SH * 60 + SM
+    end = EH * 60 + EM
+    duration = end - start 
+
+    if duration < 0:
+        duration += 24 * 60
+        
+    hours = duration // 60 #it removes the decimal part
+    minutes = duration % 60
+
+    print(f"You worked {hours} hr {minutes} min")
