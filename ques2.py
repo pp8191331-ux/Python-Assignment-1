886c4444b71f78d1cac54d049c10297637efe5cc
pp8191331-ux <pp8191331@gmail.com>
2025-10-26 22:47:38 +0530

    Add files via upload

---
+++ ques2.py
@@ -0,0 +1,7 @@
+# Given three numbers find the maximum of three numbers using the ternary operator.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+max = "first number" if (a > b and a > c) else "Second number" if b > c else "Third number"
+print("Greatest number is : ",max)
